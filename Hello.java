cb747c2a58e35138ad4705a508eb8272fa3b426a
Le <lechen614@gmail.com>
2022-03-05 15:40:22 -0500

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// Methods to run this file
+
+// 1. Run button in VS code right above main function
+// 2. `java filename`. E.g. `java Hello.java`
+// 3. First complie by `javac filename`. E.g. `javac Hello.java`
+//      Then run by `java filename_without_extension`. E.g. `java Hello`
+
+
+// Debug: Just use the "Debug" button in VScode for now.
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n");
+    }
+}
